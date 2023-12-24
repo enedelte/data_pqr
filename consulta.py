18eaf871199df0a3c6abf270391699b9bd13895c
daalejoar@gmail.com <daalejoar@gmail.com>
2023-12-24 15:48:25 -0500

    actualización data

--- consulta.py
+++ consulta.py
@@ -1,11 +1,11 @@
-import filtro_cert_1427, filtro_reg_1427
+from datos_pqr import datos_cert_1427, reg_1427
 import pandas as pd
 import numpy as np
 
 
 
-# Filtrar valores nulos y duplicados en 'Documento'
-datos_fil = datos[['Documento']].copy()
+# Filtrar valores nulos y duplicados en 'Documento' en carpeta de Certficados 1427
+datos_fil = datos_cert_1427[['Documento']].copy()
 datos_fil['Documento'] = datos_fil['Documento'].replace('None', pd.NA)  # Reemplazar 'None' por NaN
 datos_fil.dropna(subset=['Documento'], inplace=True)  # Eliminar filas con valores NaN en 'Documento'
 
@@ -19,9 +19,13 @@ conteo_repetidos = df_repetidos['Documento'].value_counts().reset_index()
 conteo_repetidos.columns = ['Documento', 'Cantidad']
 
 # Imprimir resultados
-print('Cantidad de Documentos Registrados:', cantidad_registrados)
-print('Cantidad de Documentos Repetidos:', cantidad_repetidos)
-print('Lista de Documentos Repetidos:\n', conteo_repetidos)
+print('Cantidad de Documentos Registrados en carpeta de Certificados 1427:\n', cantidad_registrados)
+print('Cantidad de Documentos Repetidos en carpeta de Certificados 1427:\n', cantidad_repetidos)
+print('Lista de Documentos Repetidos\n en carpeta de Certificados 1427:\n', conteo_repetidos)
+
+# Filtrar valores nulos y duplicados en 'Documento' en Archivo de solicitud Certficados 1427
 
 
-print(datos)
+print('Tabla de registros:\n', reg_1427)
+
+
--- datos_pqr.py
+++ datos_pqr.py
@@ -6,12 +6,57 @@ import warnings
 warnings.simplefilter('ignore')
 
 #conectores 
-pqr_datos = (r"C:\Users\dapache\OneDrive - NUEVA EPS\CERTIFICADOS DECRECTO_1427_PQRS\consolidado.txt")
-reg_datos = (r"C:\Users\dapache\OneDrive - NUEVA EPS\CERTIFICADOS DECRECTO_1427_PQRS\certificados_1427_PQRS.xlsx")
-
+pqr_datos = os.path.join(r"C:\Users\dapache\OneDrive - NUEVA EPS\CERTIFICADOS DECRECTO_1427_PQRS", "consolidado.txt")
+reg_datos = os.path.join(r"C:\Users\dapache\OneDrive - NUEVA EPS\CERTIFICADOS DECRECTO_1427_PQRS", "certificados_1427_PQRS.xlsx")
 
 #Lectura de datos
-cert_1427 = pd.read_csv(pqr_datos, sep= ';', encoding="ISO-8859-1", index_col=None, header=0) 
-reg_1427 = pd.read_excel(reg_datos, 
-                         sheet_name='CERTIFICADO_PQRS', 
-                         header=0)
+try:
+    cert_1427 = pd.read_csv(pqr_datos, sep=';', encoding='ISO-8859-1', index_col=None, header=0)
+except FileNotFoundError:
+    raise FileNotFoundError(f"No se encontró el archivo CSV en {pqr_datos}.")
+
+try:
+    reg_1427 = pd.read_excel(reg_datos, sheet_name='CERTIFICADO_PQRS', header=0)
+except FileNotFoundError:
+    print(f"Error: No se encontró el archivo Excel en {reg_datos}.")
+    reg_1427 = pd.DataFrame()
+
+# Convertir la columna 'FECHA_LIMITE' al formato de fecha
+reg_1427['FECHA_LIMITE'] = pd.to_datetime(reg_1427['FECHA_LIMITE'], errors='coerce')
+reg_1427['FECHA_DE_REMISION'] = pd.to_datetime(reg_1427['FECHA_DE_REMISION'], errors='coerce')
+reg_1427['FECHA_INICIO'] = pd.to_datetime(reg_1427['FECHA_INICIO'], errors='coerce')
+reg_1427['FECHA_FINAL'] = pd.to_datetime(reg_1427['FECHA_FINAL'], errors='coerce')
+reg_1427['FECHA_DE_ENVIO_A_DPE'] = pd.to_datetime(reg_1427['FECHA_DE_ENVIO_A_DPE'], errors='coerce')
+reg_1427['FECHA_DE_RESPUESTA_DPE'] = pd.to_datetime(reg_1427['FECHA_DE_RESPUESTA_DPE'], errors='coerce')
+
+# Formatear los valores de fechas en dataframe
+reg_1427['FECHA_LIMITE'] = reg_1427['FECHA_LIMITE'].dt.strftime('%d/%m/%Y')
+reg_1427['FECHA_DE_REMISION'] = reg_1427['FECHA_DE_REMISION'].dt.strftime('%d/%m/%Y')
+reg_1427['FECHA_INICIO'] = reg_1427['FECHA_INICIO'].dt.strftime('%d/%m/%Y')
+reg_1427['FECHA_FINAL'] = reg_1427['FECHA_FINAL'].dt.strftime('%d/%m/%Y')
+reg_1427['FECHA_DE_ENVIO_A_DPE'] = reg_1427['FECHA_DE_ENVIO_A_DPE'].dt.strftime('%d/%m/%Y')
+reg_1427['FECHA_DE_RESPUESTA_DPE'] = reg_1427['FECHA_DE_RESPUESTA_DPE'].dt.strftime('%d/%m/%Y')
+
+# Formatear los valores nulos y floats to int
+reg_1427['DOCUMENTO'] = pd.to_numeric(reg_1427['DOCUMENTO'], errors='coerce')
+reg_1427 = reg_1427.dropna(subset=['DOCUMENTO'])
+reg_1427['DOCUMENTO'] = reg_1427['DOCUMENTO'].astype(int)
+
+reg_1427['NUMERO_PQR'] = pd.to_numeric(reg_1427['NUMERO_PQR'], errors='coerce')
+reg_1427 = reg_1427.dropna(subset=['NUMERO_PQR'])
+reg_1427['NUMERO_PQR'] = reg_1427['NUMERO_PQR'].astype(int)
+
+reg_1427['N_NCAPACIDAD_INICIAL'] = pd.to_numeric(reg_1427['N_NCAPACIDAD_INICIAL'], errors='coerce')
+reg_1427 = reg_1427.dropna(subset=['N_NCAPACIDAD_INICIAL'])
+reg_1427['N_NCAPACIDAD_INICIAL'] = reg_1427['N_NCAPACIDAD_INICIAL'].astype(int)
+
+reg_1427['N_INCAPACIDAD_FINAL'] = pd.to_numeric(reg_1427['N_INCAPACIDAD_FINAL'], errors='coerce')
+reg_1427 = reg_1427.dropna(subset=['N_INCAPACIDAD_FINAL'])
+reg_1427['N_INCAPACIDAD_FINAL'] = reg_1427['N_INCAPACIDAD_FINAL'].astype(int)
+
+# Crear DataFrame a partir de cert_1427
+datos_cert_1427 = pd.DataFrame(cert_1427)
+# Poner nombre a la columna de datos
+datos_cert_1427.columns = ['Datos']
+# Dividir la columna 'Datos' en columnas separadas
+datos_cert_1427[['TD', 'Documento', 'Nombre1', 'Nombre2', 'Nombre3', 'Apellido1', 'Apellido2', 'Apellido3']] = datos_cert_1427['Datos'].str.split(' ', expand=True, n=7)
